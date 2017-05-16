80fd8210570fd304721f5968c0691b24b7e7f4c1
Joseph Maarek <josephmaarek@gmail.com>
2017-05-16 11:17:58 +0200

    Add timeout option, and gzip compression

--- src/Command/BackupSqlCommand.php
+++ src/Command/BackupSqlCommand.php
@@ -3,22 +3,18 @@
 namespace Jma\BackupBundle\Command;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\ORM\Tools\Export\ClassMetadataExporter;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Doctrine\Bundle\DoctrineBundle\Registry;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 /**
- * Class SqlExportCommand
+ * Class SqlExportCommand.
  *
  * Lets make a backup of the database
  *
- * @package Jma\BackupBundle\Command
  * @author Maarek Joseph <josephmaarek@gmail.com>
  */
 class BackupSqlCommand extends ContainerAwareCommand
@@ -29,10 +25,12 @@ class BackupSqlCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName("backup:sql")
+            ->setName('backup:sql')
             ->addOption('bin', 'b', InputOption::VALUE_OPTIONAL, 'The path to binary mysqldump', 'mysqldump')
-            ->setDescription("Create a backup file of the database in the backup directory")
-            ->setHelp("The path to binary mysqldump on Mac OSX can be \"/Applications/MAMP/Library/bin/mysqldump\"");
+            ->addOption('timeout', 't', InputOption::VALUE_OPTIONAL, 'The maximum timeout for mysqldump', null)
+            ->addOption('no-gzip', null, InputOption::VALUE_NONE, 'Disabled gzip compression')
+            ->setDescription('Create a backup file of the database in the backup directory')
+            ->setHelp('The path to binary mysqldump on Mac OSX can be "/Applications/MAMP/Library/bin/mysqldump"');
     }
 
     /**
@@ -44,6 +42,7 @@ class BackupSqlCommand extends ContainerAwareCommand
     {
         $connection = $this->getContainer()->get('doctrine')->getConnection();
         /** @var $connection Connection */
+        $withGzip = !$input->getOption('no-gzip');
 
         $driver = $connection->getDriver()->getName();
         $bin = $input->getOption('bin');
@@ -53,18 +52,18 @@ class BackupSqlCommand extends ContainerAwareCommand
         $pass = $connection->getPassword();
         $username = $connection->getUsername();
 
-        if ($driver != "pdo_mysql") {
+        if ($driver != 'pdo_mysql') {
             throw new \InvalidArgumentException('Le driver doit être pdo_mysql.');
         }
 
         $fs = new Filesystem();
-        $dir = $this->getContainer()->getParameter("jma_backup.dir");
+        $dir = $this->getContainer()->getParameter('jma_backup.dir');
         if (false === $fs->exists($dir)) {
             $fs->mkdir($dir);
         }
 
-        $filename = strftime("%Y%m%d-%H%M%S") . ".sql";
-        $out = $dir . DIRECTORY_SEPARATOR . $filename;
+        $filename = strftime('%Y%m%d-%H%M%S').'.sql'.($withGzip ? '.gz' : '');
+        $out = $dir.DIRECTORY_SEPARATOR.$filename;
 
         $opts = [
             "-h $host",
@@ -75,10 +74,16 @@ class BackupSqlCommand extends ContainerAwareCommand
             $opts[] = "-P $port";
         }
 
-        $verboseOpt = $output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE ? "-v" : "";
-        $command = "$bin " . implode(' ', $opts) . " $database $verboseOpt > $out";
+        $verboseOpt = $output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE ? '-v' : '';
+
+        $gzipCommand = "";
+        if ($withGzip) {
+            $gzipCommand = "| gzip";
+        }
+        $command = "$bin ".implode(' ', $opts)." $database $verboseOpt $gzipCommand > $out";
         $output->writeln($command);
         $process = new Process($command);
+        $process->setTimeout($input->getOption('timeout'));
         $process->run(function ($type, $buffer) use ($output, $out) {
             if ('err' === $type) {
                 $output->write(sprintf('<error>%s</error>', $buffer));
@@ -87,8 +92,8 @@ class BackupSqlCommand extends ContainerAwareCommand
             }
         });
 
-        if ($process->isSuccessful() && $output->getVerbosity() >= OutputInterface::VERBOSITY_DEBUG) {
-            $output->writeln("<info>" . file_get_contents($out) . "</info>");
+        if (!$withGzip && $process->isSuccessful() && $output->getVerbosity() >= OutputInterface::VERBOSITY_DEBUG) {
+            $output->writeln('<info>'.file_get_contents($out).'</info>');
         }
     }
-} 
+}
